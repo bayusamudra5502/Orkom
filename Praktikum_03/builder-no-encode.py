b576c8d434709d361803b56e2ac9ae2dfbd42772
bayusamudra5502 <contact@bayusamudra.my.id>
2021-12-07 20:22:19 +0700

    Menambahkan Commit Linker

--- Praktikum_03/builder-no-encode.py
+++ Praktikum_03/builder-no-encode.py
@@ -5,29 +5,29 @@ for i in upper:
         # 0x62f99261
         print("6192f962",end="")
         continue
-    if i < "M":
+    if i < "H":
         print("90"*4, end="")
         continue
 
-    if i > "M" and i < "P":
+    if i > "H" and i < "P":
        continue
 
-    #if i == "H":
-        #print("31c05068", end="")
-        #print("2f2f7368", end="")
-        #print("682f6269", end="")
-        #print("6e89e389", end="")
-        #print("c189c2b0", end="")
-        #print("0bcd8031", end="")
-        #print("c040cd80", end="")
-        #print("c3909090", end="")
-        #continue
-
-    if i == "M":
-        print("c7406c2b", end="")
-        print("fb6990c3", end="")
-        print("90909090", end="")
+    if i == "H":
+        print("31c05068", end="")
+        print("2f2f7368", end="")
+        print("682f6269", end="")
+        print("6e89e389", end="")
+        print("c189c2b0", end="")
+        print("0bcd8031", end="")
+        print("c040cd80", end="")
+        print("c3909090", end="")
         continue
+
+    #if i == "M":
+        #print("c7406c2b", end="")
+        #print("fb6990c3", end="")
+        #print("90909090", end="")
+        #continue
 
     if i != 'P' and i != 'T':
         print(str(hex(ord(i)))[2:]*4 ,end="")
@@ -36,7 +36,7 @@ for i in upper:
         # 0xffffccfc
         # 0x08049683
         print("f1960408", end="")
-        print("a0ccffff",end="")
+        print("a0dbffff",end="")
         print("83960408",end="")
     else:
         # 0x30307761
